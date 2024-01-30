b87a5a0b98e7a0a59897f9e840e57f09a10de636
sandroivankio <82328654+sandroivankio@users.noreply.github.com>
2024-01-30 11:24:34 +0000

    Add files via upload
    Initial Commit - Red LED's turning on in a vertical sequence.

---
+++ LED_64471_v2.X/build/default/production/mcc_generated_files/spi/src/spi1.p1.d
@@ -0,0 +1,4 @@
+build/default/production/mcc_generated_files/spi/src/spi1.p1:  \
+mcc_generated_files/spi/src/spi1.c  \
+mcc_generated_files/spi/src/../spi1.h  \
+mcc_generated_files/spi/src/../spi_interface.h 
